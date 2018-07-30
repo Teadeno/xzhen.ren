547139edc932da20529b6a5eb22f04abf51963e3
Teadeno <Teadeno@163.com>
2018-07-30 10:31:03 +0800

    Update database.php

--- app/conf/database.php
+++ app/conf/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname' => '127.0.0.1',
     // 数据库名
-    'database' => 'obj',
+    'database' => '',
     // 用户名
-    'username' => 'obj',
+    'username' => '',
     // 密码
-    'password' => 'Root2018@',
+    'password' => '@',
     // 端口
     'hostport' => '3306',
     // 连接dsn
